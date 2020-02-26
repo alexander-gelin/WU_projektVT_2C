8245e6a2a26dd82b975d4efad840548c20b87ef0
alexander-gelin <alexander.gelin@elev.ga.ntig.se>
2020-02-26 14:30:50 +0100

    Knapplänkar samt slideshow m.m.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ function toggleMeny() {
 }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slideimages");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); 
+}
